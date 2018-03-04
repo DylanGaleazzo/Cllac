f8b0b96fcc18330540514f3e22298c43e69b4c09
Caden Lackey <caden.lackey7@gmail.com>
2018-03-03 22:08:06 -0700

    Set up Network Test Scene
    Build project as an .exe, run it, hit play in Unity. Now you have two instances of the game running, so Host on one and Join on the other. This will demonstrate how networking connects the two clients.

---
+++ Cllac Game/Assets/Standard Assets/Scripts/CubeMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CubeMove : NetworkBehaviour {
+
+    private void Update()
+    {
+        transform.GetComponent<ConstantForce>().torque = new Vector3(0, 0, Input.GetAxis("Horizontal"));
+    }
+}
