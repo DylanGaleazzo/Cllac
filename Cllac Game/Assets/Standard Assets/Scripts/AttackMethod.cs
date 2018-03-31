139f9e7e31fa3077ed5af8763ee0ec83e18623de
Caden Lackey <caden.lackey7@gmail.com>
2018-03-31 16:19:28 -0700

    Started AttackMethod Thing

---
+++ Cllac Game/Assets/Standard Assets/Scripts/AttackMethod.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackMethod : MonoBehaviour {
+
+    string m_name, m_type;
+    int m_damage, m_level;
+    Texture2D m_image;
+
+    public AttackMethod(string name, string type, int damage, int level, Texture2D image)
+    {
+        m_name = name;
+        m_type = type;
+        m_damage = damage;
+        m_level = level;
+        m_image = image;
+    }
+
+
+}
